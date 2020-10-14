a638b2a2330501369606737434a20a4cf8500c0b
Amy Burnett <16aburnett@gmail.com>
2020-10-14 15:40:03 -0400

    created code directory

---
+++ code/responsebot.py
@@ -0,0 +1,19 @@
+# ResponseBot program
+# Desc:   This program acts as the core to the response bot.
+#   This accepts input and uses other modules to generate and
+#   return a response for the input. 
+# Author: Amy Burnett
+# Date:   2020/10/13
+##########################################################################
+
+class ResponseBot:
+
+    def __init__(self):
+        pass
+
+##########################################################################
+
+    def respond(self, input):
+        return "I am but an empty mind!"
+
+##########################################################################
