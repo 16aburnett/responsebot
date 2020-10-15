5c978dc608ca21befb7fd0e29c0a13168bd62bc2
Amy Burnett <16aburnett@gmail.com>
2020-10-15 00:16:10 -0400

    Added gitignore and a sample test for tensorflow and keras

--- code/POSTagger/POSTagger.py
+++ code/POSTagger/POSTagger.py
@@ -1,7 +1,35 @@
 
+# Test example from https://keras.io/examples/nlp/bidirectional_lstm_imdb/
 
 import numpy as np
 from tensorflow import keras
 from tensorflow.keras import layers
 
-print("it worked!")
+
+max_features = 20000  # Only consider the top 20k words
+maxlen = 200  # Only consider the first 200 words of each movie review
+
+# Input for variable-length sequences of integers
+inputs = keras.Input(shape=(None,), dtype="int32")
+# Embed each integer in a 128-dimensional vector
+x = layers.Embedding(max_features, 128)(inputs)
+# Add 2 bidirectional LSTMs
+x = layers.Bidirectional(layers.LSTM(64, return_sequences=True))(x)
+x = layers.Bidirectional(layers.LSTM(64))(x)
+# Add a classifier
+outputs = layers.Dense(1, activation="sigmoid")(x)
+model = keras.Model(inputs, outputs)
+model.summary()
+
+# Load the IMDB movie review sentiment data
+(x_train, y_train), (x_val, y_val) = keras.datasets.imdb.load_data(
+    num_words=max_features
+)
+print(len(x_train), "Training sequences")
+print(len(x_val), "Validation sequences")
+x_train = keras.preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
+x_val = keras.preprocessing.sequence.pad_sequences(x_val, maxlen=maxlen)
+
+# train and eval 
+model.compile("adam", "binary_crossentropy", metrics=["accuracy"])
+model.fit(x_train, y_train, batch_size=32, epochs=2, validation_data=(x_val, y_val))
